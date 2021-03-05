c2ab9383e6efc8d46937d51fabcd97449daf9461
Wilfried Bounsi <wilcoln99@gmail.com>
2021-03-05 13:51:56 +0100

    remove ignored files from git tracking

--- publish.sh
+++
@@ -1,7 +0,0 @@
-username='wilcoln'
-password='g5XfiasLxxpLNXS'
-# rm -rf build dist *.egg-info
-python setup.py sdist bdist_wheel
-#python3 -m twine upload dist/*
-python -m twine upload --repository-url https://pypi.org/legacy/ dist/* || rm -rf build dist *.egg-info
-# rm -rf build dist *.egg-info
